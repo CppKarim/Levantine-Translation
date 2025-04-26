0dc22ced9290bfe4d6a163aebf821345f40ebe7a
CppKarim <kelhus2@uic.edu>
2025-04-25 22:59:44 -0500

    training code

--- src/finetune.sh
+++ src/finetune.sh
@@ -8,9 +8,10 @@ export PYTHONPATH="./"
 
 # Script
 echo "This script trains an LLM using supervised fine tuining and a dataset of text."
-#accelerate config
-CCL_TOPO_FABRIC_VERTEX_CONNECTION_CHECK=1 accelerate launch src/training/supervised_finetuning.py\
-    --model_name openai-community/gpt2 --dataset "KHuss/UFAL_levantine_processed" \
+accelerate config
+export CCL_TOPO_FABRIC_VERTEX_CONNECTION_CHECK=1 
+accelerate launch src/training/supervised_finetuning.py\
+    --model_name openai-community/gpt2 --dataset "data\dataset\Huggingface\finetune_roman" \
     --dataset_offset 0 --dataset_percent 100 --save_interval 0.02 --dataset_num_proc 128 \
     --learning_rate 7e-5 --seq_len 1024 --per_device_batch 64 --gradient_accumulation_steps 1 --epochs 3 \
-    --subdirectory sft/arab --save_model \
+    --subdirectory sft/roman --save_model \
--- src/seq_2_seq.sh
+++ src/seq_2_seq.sh
@@ -7,9 +7,10 @@ export PYTHONPATH="./"
 
 # Script
 echo "This script trains an LLM using supervised fine tuining and a dataset of text."
-#accelerate config
-CCL_TOPO_FABRIC_VERTEX_CONNECTION_CHECK=1 accelerate launch src/training/seq_2_seq.py\
-    --model_name KHuss/gpt2_sft_levantine_bw --dataset "KHuss/UFAL_levantine_buckwater_seq2seq" \
+accelerate config
+export CCL_TOPO_FABRIC_VERTEX_CONNECTION_CHECK=1 
+accelerate launch src/training/seq_2_seq.py\
+    --model_name data/models/gpt2/sft/roman --dataset "data\dataset\Huggingface\translate_roman" \
     --dataset_offset 0 --dataset_percent 100 --save_interval 0.02 --dataset_num_proc 128 \
     --learning_rate 7e-5 --seq_len 512 --per_device_batch 32 --gradient_accumulation_steps 1 --epochs 3 \
-    --subdirectory seq2seq/bw --save_model \
+    --subdirectory translate/roman --save_model \
